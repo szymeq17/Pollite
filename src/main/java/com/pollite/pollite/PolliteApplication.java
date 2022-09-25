09bfea0f7f15ddd910493a5a96f7b801f3c96c1c
szymeq17 <rysz.szymon@gmail.com>
2022-09-25 17:42:48 +0200

    Initial commit

---
+++ src/main/java/com/pollite/pollite/PolliteApplication.java
@@ -0,0 +1,13 @@
+package com.pollite.pollite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pollite/pollite/PolliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollite.pollite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
