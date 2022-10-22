03919d88070e37c81ab65af138e5823e1065df24
szymeq17 <rysz.szymon@gmail.com>
2022-10-22 16:49:40 +0200

    Add SurveyQuestionExclusion model

--- src/main/java/com/pollite/pollite/dto/SurveyConfigurationDto.java
+++ src/main/java/com/pollite/pollite/dto/SurveyConfigurationDto.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 
 @Data
 @Builder
@@ -16,4 +17,6 @@ public class SurveyConfigurationDto {
     private OffsetDateTime startDate;
 
     private OffsetDateTime endDate;
+
+    private List<SurveyQuestionExclusionDto> exclusions;
 }
--- src/main/java/com/pollite/pollite/model/survey/SurveyQuestionExclusion.java
+++ src/main/java/com/pollite/pollite/model/survey/SurveyQuestionExclusion.java
@@ -6,35 +6,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.time.OffsetDateTime;
-import java.util.List;
 
 @Entity
-@Table(name = "survey_configuration")
+@Table(name = "survey_question_exclusion")
 @Builder
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class SurveyConfiguration {
+public class SurveyQuestionExclusion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Boolean isActive;
+    private Long questionOrder;
 
-    private OffsetDateTime startDate;
+    private Long answerOrder;
 
-    private OffsetDateTime endDate;
-
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<SurveyQuestionExclusion> exclusions;
+    private Long excludedQuestionOrder;
 }
