2a884e439413ecd6eaf82316f854b53bff8bf0b0
szymeq17 <rysz.szymon@gmail.com>
2023-01-16 23:41:02 +0100

    Implement selecting answers

--- frontend/pollite/src/app/survey/complete-survey-form/complete-survey-form.component.ts
+++ frontend/pollite/src/app/survey/complete-survey-form/complete-survey-form.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
 import {SurveyService} from "../service/survey.service";
-import {Survey} from "../../model/Survey";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {Survey, SurveyQuestion} from "../../model/Survey";
+import {AbstractControl, FormArray, FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {MatCheckboxChange} from "@angular/material/checkbox";
+import {MatRadioChange} from "@angular/material/radio";
 
 @Component({
   selector: 'app-complete-survey-form',
@@ -15,7 +17,7 @@ export class CompleteSurveyFormComponent implements OnInit {
               private route: ActivatedRoute,
               private surveyService: SurveyService) { }
   survey: Survey;
-  form: FormGroup;
+  form: FormGroup = new FormGroup({});
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
@@ -23,25 +25,62 @@ export class CompleteSurveyFormComponent implements OnInit {
       this.surveyService.getSurvey(surveyId).subscribe(response => {
         this.survey = response;
         this.survey.description = "Przykładowy opis ankiety. Bla bla bla bla";
-      });
 
-      this.form = this.fb.group({
-        surveyId: surveyId,
-        completedQuestions: this.fb.array([])
-      })
+        this.form = this.fb.group({
+          surveyId: surveyId,
+          completedQuestions: this.fb.array(this.buildQuestionGroups())
+        })
+      });
     });
+  }
+
+  get completedQuestionForms(): FormArray {
+    return this.form.get('completedQuestions') as FormArray;
+  }
+
+  completedQuestionAnswersForms(questionIndex: number): FormArray {
+    return this.completedQuestionForms.at(questionIndex).get('answers') as FormArray
   }
 
   onSubmit() {
     console.log(this.form.value);
   }
 
-  // private buildQuestions(): FormArray {
-  //   return this.survey.questions.map()
-  // }
-  //
-  // private buildQuestionAnswer(answer: SurveyQuestionAnswer) {
-  //   return this.fb.control()
-  // }
+  onCheckboxChange(e: MatCheckboxChange, questionIndex: number) {
+    const answers: FormArray = this.completedQuestionAnswersForms(questionIndex);
+    const value: number = Number.parseInt(e.source.value);
+    if (e.checked) {
+      answers.push(new FormControl(value));
+    } else {
+      let i = 0;
+      answers.controls.forEach((item: AbstractControl) => {
+        if (item.value === value) {
+          answers.removeAt(i);
+          return;
+        }
+        i++;
+      })
+    }
+  }
+
+  onRadioChange(e: MatRadioChange, questionIndex: number) {
+    const answers: FormArray = this.completedQuestionAnswersForms(questionIndex);
+    answers.clear();
+    answers.push(new FormControl(e.value));
+  }
+
+  private buildQuestionGroups(): FormGroup[] {
+    return this.survey.questions.map(question => this.buildCompletedQuestion(question));
+  }
+
+  private buildCompletedQuestion(question: SurveyQuestion) {
+    return this.fb.group({
+      questionId: question.id,
+      text: question.text,
+      order: question.order,
+      type: question.type,
+      answers: this.fb.array([])
+    });
+  }
 
 }
